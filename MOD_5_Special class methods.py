a7726045a677d6f9e0b45bdd730c44f814835d8e
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-06-21 11:13:45 +0300

    Add files via upload

---
+++ MOD_5_Special class methods.py
@@ -0,0 +1,32 @@
+
+
+# Домашнее задание по уроку "Специальные методы классов"
+
+
+
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print(self.numberOfFloors)
+
+house = House()
+house.setNewNumberOfFloors(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
